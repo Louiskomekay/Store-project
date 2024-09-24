6988260ba672b795a507e8b51a7e732378569cd2
Louiskome <okwoho.kome@gmail.com>
2024-09-24 11:18:39 +0100

    Created all the pages and installed all the shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant='outline' size='lg' className="capitalize m-8">Click here</Button>
+    </div>
   )
 }
 
