8b8cb3d5827484958a593a4343d077276acac999
Louiskome <okwoho.kome@gmail.com>
2024-10-19 22:21:02 +0100

    setup the userIcon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,18 @@
+import { LuUser2 } from "react-icons/lu"
+import { currentUser } from "@clerk/nextjs/server"
 
-function UserIcon() {
+async function UserIcon() {
+    const user = await currentUser();
+    const profileImage = user?.imageUrl;
+
+    if (profileImage) {
+        return (
+            <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+        )
+    }
+
     return (
-        <div>UserIcon</div>
+        <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
     )
 }
 
