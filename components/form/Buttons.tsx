b97f72b7afb2e285fb458103b28ac624d1df1b35
Louiskome <okwoho.kome@gmail.com>
2024-10-22 23:38:05 +0100

    Completed the setup for the submit button component

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,8 +1,27 @@
+'use client';
 
-function Buttons() {
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { LuTrash2, LuPenSquare } from 'react-icons/lu';
+
+type btnSize = 'default' | 'lg' | 'sm';
+
+type SubmitButtonProps = {
+    className?: string;
+    text?: string;
+    size?: btnSize;
+};
+
+function SubmitButton({ className = '', text = 'submit', size = 'lg' }: SubmitButtonProps) {
+    const { pending } = useFormStatus();
+
     return (
-        <div>Buttons</div>
+        <Button type='submit' disabled={pending} className={cn('capitalize', className)} size={size}>{pending ? <> <ReloadIcon className='mr-2 w-4 h-4 animate-spin' /> Please wait... </> : text}</Button>
     )
 }
 
-export default Buttons
+export default SubmitButton;
